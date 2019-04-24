0a6ab472a7cb46180621d888288ba47ddb1ff7db
oscarta3 <47740554+oscarta3@users.noreply.github.com>
2019-04-24 17:15:49 +0200

    Player added (ModulePlayer.cpp + ModulePlayer.h)

--- SamuraiShodown/SamuraiShodown/ModulePlayer.cpp
+++ SamuraiShodown/SamuraiShodown/ModulePlayer.cpp
@@ -2,30 +2,54 @@
 #include "Application.h"
 #include "ModuleTextures.h"
 #include "ModuleInput.h"
-#include "ModuleParticles.h"
 #include "ModuleRender.h"
-#include "ModuleCollision.h"
-#include "ModuleFadeToBlack.h"
 #include "ModulePlayer.h"
 
 // Reference at https://www.youtube.com/watch?v=OEhmUuehGOA
 
 ModulePlayer::ModulePlayer()
 {
-	// idle animation (just the ship)
-	idle.PushBack({66, 1, 32, 14});
+	position.x = 100;
+	position.y = 220;
 
-	// move upwards
-	up.PushBack({100, 1, 32, 14});
-	up.PushBack({132, 0, 32, 14});
-	up.loop = false;
-	up.speed = 0.1f;
+	// idle animation
+	idle.PushBack({22, 0, 73, 118});
+	idle.PushBack({96, 0, 73, 118});
+	idle.PushBack({169, 0, 72, 118});
+	idle.PushBack({241, 0, 72, 118});
+	idle.PushBack({169, 0, 72, 118 });
+	idle.PushBack({96, 0, 73, 118 });
+	idle.speed = 0.05f;
 
-	// Move down
-	down.PushBack({33, 1, 32, 14});
-	down.PushBack({0, 1, 32, 14});
-	down.loop = false;
-	down.speed = 0.1f;
+	// walk forward animation 
+	forward.PushBack({313, 0, 68, 118});
+	forward.PushBack({446, 0, 58, 118});
+	forward.PushBack({576, 0, 56, 118});
+	forward.PushBack({701, 0, 61, 118});
+	forward.PushBack({823, 0, 69, 118});
+	forward.PushBack({946, 0, 75, 118});
+	forward.PushBack({823, 0, 69, 118});
+	forward.PushBack({701, 0, 61, 118});
+	forward.PushBack({576, 0, 56, 118});
+	forward.PushBack({446, 0, 58, 118});
+	forward.speed = 0.05f;
+
+	// walk backward animation 
+	backward.PushBack({ 1023, 0, 82, 118 });
+	backward.PushBack({ 1109, 0, 74, 118 });
+	backward.PushBack({ 1187, 0, 68, 118 });
+	backward.PushBack({ 1259, 0, 64, 118 });
+	backward.PushBack({ 1326, 0, 69, 118 });
+	backward.PushBack({ 1399, 0, 74, 118 });
+	backward.PushBack({ 1326, 0, 69, 118 });
+	backward.PushBack({ 1259, 0, 64, 118 });
+	backward.PushBack({ 1187, 0, 68, 118 });
+	backward.PushBack({ 1109, 0, 74, 118 });
+	backward.speed = 0.05f;
+
+
+
+	
 }
 
 ModulePlayer::~ModulePlayer()
@@ -34,78 +58,35 @@ ModulePlayer::~ModulePlayer()
 // Load assets
 bool ModulePlayer::Start()
 {
-	LOG("Loading player");
-
-	graphics = App->textures->Load("rtype/ship.png");
-
-	position.x = 150;
-	position.y = 120;
-
-	// TODO 2: Add a collider to the player
-
-	return true;
-}
-
-// Unload assets
-bool ModulePlayer::CleanUp()
-{
-	LOG("Unloading player");
-
-	App->textures->Unload(graphics);
-
-	return true;
+	LOG("Loading player textures");
+	bool ret = true;
+	graphics = App->textures->Load("Haohmaru.png"); // arcade version
+	return ret;
 }
 
 // Update: draw background
 update_status ModulePlayer::Update()
 {
+	Animation* current_animation = &idle;
+
 	int speed = 1;
 
-	if(App->input->keyboard[SDL_SCANCODE_A] == KEY_STATE::KEY_REPEAT)
+	if(App->input->keyboard[SDL_SCANCODE_D] == 1)
 	{
-		position.x -= speed;
-	}
-
-	if(App->input->keyboard[SDL_SCANCODE_D] == KEY_STATE::KEY_REPEAT)
-	{
+		current_animation = &forward;
 		position.x += speed;
 	}
 
-	if(App->input->keyboard[SDL_SCANCODE_S] == KEY_STATE::KEY_REPEAT)
+	if (App->input->keyboard[SDL_SCANCODE_A] == 1)
 	{
-		position.y += speed;
-		if(current_animation != &down)
-		{
-			down.Reset();
-			current_animation = &down;
-		}
+		current_animation = &backward;
+		position.x -= speed;
 	}
-
-	if(App->input->keyboard[SDL_SCANCODE_W] == KEY_STATE::KEY_REPEAT)
-	{
-		position.y -= speed;
-		if(current_animation != &up)
-		{
-			up.Reset();
-			current_animation = &up;
-		}
-	}
-
-	if(App->input->keyboard[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN)
-	{
-		App->particles->AddParticle(App->particles->laser, position.x + 20, position.y, COLLIDER_PLAYER_SHOT);
-	}
-
-	if(App->input->keyboard[SDL_SCANCODE_S] == KEY_STATE::KEY_IDLE
-	   && App->input->keyboard[SDL_SCANCODE_W] == KEY_STATE::KEY_IDLE)
-		current_animation = &idle;
-
-	// TODO 3: Update collider position to player position
 
 	// Draw everything --------------------------------------
-	App->render->Blit(graphics, position.x, position.y, &(current_animation->GetCurrentFrame()));
+	SDL_Rect r = current_animation->GetCurrentFrame();
 
+	App->render->Blit(graphics, position.x, position.y - r.h, &r);
+	
 	return UPDATE_CONTINUE;
 }
-
-// TODO 4: Detect collision with a wall. If so, go back to intro screen.
--- SamuraiShodown/SamuraiShodown/ModulePlayer.h
+++ SamuraiShodown/SamuraiShodown/ModulePlayer.h
@@ -2,11 +2,11 @@
 #define __ModulePlayer_H__
 
 #include "Module.h"
-//#include "Animation.h"
-//#include "p2Point.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
 
 struct SDL_Texture;
-struct Collider;
 
 class ModulePlayer : public Module
 {
@@ -16,16 +16,15 @@ public:
 
 	bool Start();
 	update_status Update();
-	bool CleanUp();
 
 public:
 
 	SDL_Texture* graphics = nullptr;
-	Animation* current_animation = nullptr;
 	Animation idle;
-	Animation up;
-	Animation down;
+	Animation forward;
+	Animation backward;
 	iPoint position;
+
 };
 
 #endif
