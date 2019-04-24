9840647d779dc4121ad7f2968c39c55c1fed433a
Turmo11 <Turmo11@users.noreply.github.com>
2019-04-24 11:21:10 +0200

    ds

--- SamuraiShodown/SamuraiShodown/ModuleSceneWelcome.cpp
+++ SamuraiShodown/SamuraiShodown/ModuleSceneWelcome.cpp
@@ -25,17 +25,17 @@ ModuleSceneWelcome::~ModuleSceneWelcome()
 bool ModuleSceneWelcome::Start()
 {
 
-	/*LOG("Loading Welcome scene");
+	LOG("Loading Welcome scene");
 
 	App->audio->music[2] = Mix_LoadMUS("Assets/audio/Music/NeoGeoLogo.ogg");
 
 	if (!App->audio->music[2]) {
 		LOG("Mix_LoadMUS(\"WelcomeMusic.ogg\"): %s\n", Mix_GetError());
 	}
-	else {*/
+	else {
 		graphics = App->textures->Load("Assets/Sprites/welcome.png");
-		/*Mix_PlayMusic(App->audio->music[2], 2);
-	}*/
+		Mix_PlayMusic(App->audio->music[2], 2);
+	}
 
 	
 	
@@ -49,7 +49,7 @@ bool ModuleSceneWelcome::CleanUp()
 
 	App->textures->Unload(graphics);
 	
-	//App->audio->CleanUp();
+	App->audio->CleanUp();
 
 	return true;
 }
