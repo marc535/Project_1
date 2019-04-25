eafeb949540cbac35d1ee472927dbdb556618972
Turmo11 <Turmo11@users.noreply.github.com>
2019-04-25 18:30:10 +0200

    Player added fr

---
+++ SamuraiShodown/SamuraiShodown/ModulePlayer2.cpp
@@ -0,0 +1,183 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleInput.h"
+#include "ModuleRender.h"
+#include "ModulePlayer.h"
+#include "ModuleCollision.h"
+
+ModulePlayer::ModulePlayer()
+{
+	position.x = 100;
+	position.y = 220;
+
+	// idle animation
+	idle.PushBack({22, 0, 73, 118});
+	idle.PushBack({96, 0, 73, 118});
+	idle.PushBack({169, 0, 72, 118});
+	idle.PushBack({241, 0, 72, 118});
+	idle.PushBack({169, 0, 72, 118 });
+	idle.PushBack({96, 0, 73, 118 });
+	idle.speed = 0.14f;
+
+	// walk forward animation 
+	forward.PushBack({313, 0, 68, 118});
+	forward.PushBack({446, 0, 58, 118});
+	forward.PushBack({576, 0, 56, 118});
+	forward.PushBack({701, 0, 61, 118});
+	forward.PushBack({823, 0, 69, 118});
+	forward.PushBack({946, 0, 75, 118});
+	forward.speed = 0.14f;
+
+	// walk backward animation 
+	backward.PushBack({ 1023, 0, 82, 118 });
+	backward.PushBack({ 1109, 0, 74, 118 });
+	backward.PushBack({ 1187, 0, 68, 118 });
+	backward.PushBack({ 1259, 0, 64, 118 });
+	backward.PushBack({ 1326, 0, 69, 118 });
+	backward.PushBack({ 1399, 0, 74, 118 });
+	backward.PushBack({ 1326, 0, 69, 118 });
+	backward.PushBack({ 1259, 0, 64, 118 });
+	backward.PushBack({ 1187, 0, 68, 118 });
+	backward.PushBack({ 1109, 0, 74, 118 });
+	backward.speed = 0.14f;
+
+	// jump animation
+	jump.PushBack({ 22,124,59,112 }); 
+	jump.PushBack({ 83,145,75,91 });
+	jump.PushBack({ 158,153,76,84 });
+	jump.PushBack({ 235,126,68,110 });
+	jump.speed = 0.07f;
+
+	// kick animation
+	kick.PushBack({ 305,141,66,118 });
+	kick.PushBack({ 372,138,86,118 });
+	kick.PushBack({ 305,141,66,118 });
+	//kick.PushBack({22, 0, 73, 118});
+	kick.speed = 0.14f;
+	
+}
+
+ModulePlayer::~ModulePlayer()
+{}
+
+// Load assets
+bool ModulePlayer::Start()
+{
+	LOG("Loading player textures");
+	bool ret = true;
+	graphics = App->textures->Load("Assets/Sprite_Sheets/Characters/Haohmaru/Haohmaru.png");
+	
+	p1Collider = App->collision->AddCollider({ position.x, position.y - 90, 60, 90 }, COLLIDER_PLAYER, this);
+	return ret;
+}
+
+// Update: draw background
+update_status ModulePlayer::Update()
+{
+	Animation* current_animation = &idle;
+	
+	int speed = 1;
+	float yVelocity = 15.1f;
+	float yAcceleration = 0.87f;
+	
+
+	if (App->input->keyboard[SDL_SCANCODE_D] == KEY_STATE::KEY_REPEAT)
+	{
+		if (!action && !flipPlayer) { current_animation = &forward; }
+		if (!action && flipPlayer) { current_animation = &backward; }
+		position.x += speed;
+	}
+		
+	if (App->input->keyboard[SDL_SCANCODE_A] == KEY_STATE::KEY_REPEAT)
+	{
+		if (!action && !flipPlayer) { current_animation = &backward; }
+		if (!action && flipPlayer) { current_animation = &forward; }
+		position.x -= speed;
+
+	}
+	if (App->input->keyboard[SDL_SCANCODE_W] == KEY_STATE::KEY_REPEAT && !action)
+	{
+
+		jumped = true;
+		action = true;
+		
+	}
+	if (App->input->keyboard[SDL_SCANCODE_Q] == KEY_STATE::KEY_REPEAT && !action)
+	{
+
+		kicked = true;
+		action = true;
+
+	}
+	if (App->input->keyboard[SDL_SCANCODE_DOWN] == KEY_STATE::KEY_REPEAT)
+	{
+		if (flipPlayer) { flipPlayer = false; }
+		else { flipPlayer = true; }
+		
+	}
+
+	if(action){
+	
+		if (jumped) {
+
+			current_animation = &jump;
+
+			position.y = 220 - (yVelocity*var1) + (0.5*(yAcceleration)*pow(var1, 2));
+			airborne = true;
+
+			
+			if (position.y > 220 && airborne == true)	//end of the jump
+			{
+				var1 = 0;
+				airborne = false;
+				jumped = false;
+				position.y = 220;
+				action = false;
+
+			}
+			var1++;
+
+		}
+
+		if (kicked) {
+
+			current_animation = &kick;
+			if (kick.FinishedAnimation() == true) {
+			
+				kicked = false; 
+				action = false; 
+
+				kick.finishingAnimation(false); 
+			}
+		
+		}
+	
+	
+	
+	}
+
+	p1Collider->SetPos(position.x, position.y - 90);
+
+	// Draw everything --------------------------------------
+	SDL_Rect r = current_animation->GetCurrentFrame();
+
+	App->render->Blit(graphics, position.x, position.y - r.h, &r, 1.0f, flipPlayer);
+	
+	return UPDATE_CONTINUE;
+}
+
+void ModulePlayer::OnCollision(Collider* c1, Collider* c2) {
+	if (this->p1Collider == c1) {
+		if (c1->rect.x < c2->rect.x)
+			position.x = c2->rect.x - c1->rect.w;
+		if (c1->rect.x > c2->rect.x)
+			position.x = c2->rect.x + c2->rect.w;
+	}
+
+	if (c2->type == COLLIDER_ENEMY_SHOT) {
+		/*Mix_PlayChannel(-1, App->audio->effects[2], 0);
+		health += 30;
+		getsHit = true; doingAction = true;*/
+	}
+}
---
+++ SamuraiShodown/SamuraiShodown/ModulePlayer2.h
@@ -0,0 +1,46 @@
+#ifndef __ModulePlayer_H__
+#define __ModulePlayer_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
+
+struct SDL_Texture;
+
+class ModulePlayer : public Module
+{
+public:
+	ModulePlayer();
+	~ModulePlayer();
+
+	bool Start();
+	update_status Update();
+
+	void OnCollision(Collider* c1, Collider* c2);
+
+public:
+
+	SDL_Texture* graphics = nullptr;
+
+	Collider* p1Collider = nullptr;
+	Collider* attack = nullptr;
+	Animation idle;
+	Animation forward;
+	Animation backward;
+	Animation jump;
+	Animation kick;
+
+	iPoint position;
+
+	bool jumped = false;
+	bool airborne = false;
+	bool action = false;
+	bool kicked = false;
+	bool flipPlayer = false;
+
+	int var1 = 0;
+	
+};
+
+#endif
