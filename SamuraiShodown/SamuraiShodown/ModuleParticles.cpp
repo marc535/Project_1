6fa3664d2192cbf9376bd829a566a1f034f96c14
Turmo11 <Turmo11@users.noreply.github.com>
2019-04-26 12:24:31 +0200

    Versión 0.3 finished

--- SamuraiShodown/SamuraiShodown/ModuleParticles.cpp
+++ SamuraiShodown/SamuraiShodown/ModuleParticles.cpp
@@ -90,7 +90,7 @@ update_status ModuleParticles::Update()
 		}
 		else if(SDL_GetTicks() >= p->born)
 		{
-			if (App->player->flipPlayer) { App->render->Blit(graphics, p->position.x, p->position.y, &(p->anim.GetCurrentFrame()), 1.0f, false); }
+			if (App->player->flipPlayer == true) { App->render->Blit(graphics, p->position.x, p->position.y, &(p->anim.GetCurrentFrame()), 1.0f, true); }
 			else { App->render->Blit(graphics, p->position.x, p->position.y, &(p->anim.GetCurrentFrame()), 1.0f, false); }
 			if(p->fx_played == false)
 			{
