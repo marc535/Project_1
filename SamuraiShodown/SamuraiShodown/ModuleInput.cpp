65a6094f21914a00ee76caf6db52331cce7da6f4
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2019-05-16 08:11:01 +0200

    añadido mando en el modulo de inputs

--- SamuraiShodown/SamuraiShodown/ModuleInput.cpp
+++ SamuraiShodown/SamuraiShodown/ModuleInput.cpp
@@ -5,8 +5,12 @@
 
 ModuleInput::ModuleInput() : Module()
 {
-	for (uint i = 0; i < MAX_KEYS; ++i) {
+	for (uint i = 0; i < MAX_KEYS; ++i)
 		keyboard[i] = KEY_IDLE;
+	for (uint i = 0; i < MAX_BUTTONS; ++i) {
+		for (int j = 0; j < 2; ++j) {
+			game_pad[i][j] = KEY_IDLE;
+		}
 	}
 }
 
@@ -27,6 +31,7 @@ bool ModuleInput::Init()
 		LOG("SDL_EVENTS could not initialize! SDL_Error: %s\n", SDL_GetError());
 		ret = false;
 	}
+	SDL_Init(SDL_INIT_GAMECONTROLLER);
 
 	return ret;
 }
@@ -59,6 +64,65 @@ update_status ModuleInput::PreUpdate()
 			}
 		}
 	}
+
+	for (int i = 0; i < MAX_BUTTONS; ++i) {
+		game_pad[i][GAME_PAD_1] = SDL_GameControllerGetButton(controller_player_1, (SDL_GameControllerButton)i);
+		game_pad[i][GAME_PAD_2] = SDL_GameControllerGetButton(controller_player_2, (SDL_GameControllerButton)i);
+	}
+
+	for (int i = 0; i < MAX_BUTTONS; ++i) {
+
+		for (int j = 0; j < MAX_GAME_PAD; ++j) {
+			if (game_pad[i][j] == KEY_IDLE) {
+				game_pad[i][j] == KEY_DOWN;
+				break;
+			}
+			else {
+				game_pad[i][j] == KEY_REPEAT;
+				break;
+			}
+
+			if (game_pad[i][j] == KEY_REPEAT || game_pad[i][j] == KEY_DOWN) {
+				game_pad[i][j] == KEY_UP;
+				break;
+			}
+			else {
+				game_pad[i][j] == KEY_IDLE;
+				break;
+			}
+		}
+
+	}
+	while (SDL_PollEvent(&Events) == 1) {
+
+		switch (Events.type) {
+		case SDL_CONTROLLERDEVICEADDED: {
+			int num_joystincks = SDL_NumJoysticks();
+			for (int i = 0; i < num_joystincks; ++i) {
+				if (SDL_GameControllerGetAttached(controller_player_1) == SDL_FALSE) {
+					controller_player_1 = SDL_GameControllerOpen(i);
+					break;
+				}
+				if (SDL_GameControllerGetAttached(controller_player_2) == SDL_FALSE) {
+					controller_player_2 = SDL_GameControllerOpen(i);
+					break;
+				}
+			}
+
+			break; }
+		case SDL_CONTROLLERDEVICEREMOVED:
+			if (SDL_GameControllerGetAttached(controller_player_1) == SDL_FALSE) {
+				SDL_GameControllerClose(controller_player_1);
+				controller_player_1 = nullptr;
+			}
+			if (SDL_GameControllerGetAttached(controller_player_2) == SDL_FALSE) {
+				SDL_GameControllerClose(controller_player_2);
+				controller_player_2 = nullptr;
+			}
+			break;
+		}
+	}
+
 
 	if (keyboard[SDL_SCANCODE_ESCAPE]) {
 
--- SamuraiShodown/SamuraiShodown/ModuleInput.h
+++ SamuraiShodown/SamuraiShodown/ModuleInput.h
@@ -4,8 +4,14 @@
 #include "Module.h"
 #include "Globals.h"
 #include "SDL\include\SDL_scancode.h"
+#include "SDL/include/SDL_events.h"
 
 #define MAX_KEYS 300
+#define MAX_BUTTONS 15
+
+#define GAME_PAD_1 0
+#define GAME_PAD_2 1
+#define MAX_GAME_PAD 2
 
 enum KEY_STATE
 {
@@ -28,6 +34,14 @@ public:
 
 public:
 	KEY_STATE keyboard[MAX_KEYS];
+	SDL_Event Events;
+	Uint8 game_pad[MAX_BUTTONS][MAX_GAME_PAD];
+
+	SDL_GameController* controller_player_1 = nullptr;
+	SDL_GameController* controller_player_2 = nullptr;
+
+private:
+	int controller_1_index;
 };
 
 #endif // __ModuleInput_H__
